6f30af34a16b097d5a2525609e0725351d3581b5
Robert Phamle <robertphamle@gmail.com>
2020-05-31 23:49:27 -0700

    move files from build/ to build/worker-base/

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,3 +1,3 @@
 # This script builds the base worker image
 cd ..
-docker build --pull --rm -f build/Dockerfile -t web-scraper-jobs/worker-base:latest .
+docker build --pull --rm -f build/worker-base/Dockerfile -t web-scraper-jobs/worker-base:latest .
