acf18651f8a8b85f489bd6ffb71b50c4dbc915a0
Robert Phamle <robertphamle@gmail.com>
2020-06-01 19:44:56 -0700

    rename cmd -> app

--- _scripts/deploy_linkedin.sh
+++ _scripts/deploy_linkedin.sh
@@ -1,4 +1,4 @@
 # This script deploys the base worker image, and copies the linkedin scraping application script
 cd ..
 docker run --rm -d -t --name worker-linkedin web-scraper-jobs/worker-base:latest
-docker cp cmd/linkedin/scrape.py worker-linkedin:/app
+docker cp app/linkedin/scrape.py worker-linkedin:/app
