cef8c24c054a047377f282dcb81cde4f3243f7f8
Robert Phamle <robertphamle@gmail.com>
2020-06-04 23:30:21 -0700

    modularized script into two classes

--- app/linkedin/scrape.py
+++ app/linkedin/scrape.py
@@ -8,23 +8,48 @@ from bs4 import BeautifulSoup
 search_keywords = 'energy engineer'
 search_location = 'Los Angeles Metropolitan Area'
 
-def get_url(url_base, url_vars):
-    return url_base + urllib.parse.urlencode(url_vars)
+class LinkedinBasePage():
 
-def renderWholePage(driver):
-    # Get length of current page
-    script = "window.scrollTo(0, document.body.scrollHeight);var len_page=document.body.scrollHeight;return len_page;"
-    len_page = driver.execute_script(script)
+    def __init__(self, driver, url):
+        self.driver = driver
+        self._goToUrl(url)
 
-    # Loop until all pages rendered
-    end = False
-    while(end == False):
-        previous_len_page = len_page
-        time.sleep(3)
-        len_page = driver.execute_script(script)
+    @classmethod
+    def fromArgs(cls, driver, url_base, url_args):
+        return cls(driver, url_base + urllib.parse.urlencode(url_vars))
 
-        if(previous_len_page == len_page):
-            end = True
+    def _goToUrl(self, url):
+        self.driver.get(url)
+
+    def _getSoup(self, **soup_args):
+        # Get the entire html and pass to BeautifulSoup
+        html_source = self.driver.page_source
+        soup = BeautifulSoup(html_source, **soup_args)
+
+        return soup
+
+    def scrollWholePage(self):
+        # Get length of current page
+        script = "window.scrollTo(0, document.body.scrollHeight);var len_page=document.body.scrollHeight;return len_page;"
+        len_page = self.driver.execute_script(script)
+
+        # Loop until all pages rendered
+        end = False
+        while(end == False):
+            previous_len_page = len_page
+            time.sleep(3)
+            len_page = self.driver.execute_script(script)
+
+            if(previous_len_page == len_page):
+                end = True
+
+class LinkedinMainSearchPage(LinkedinBasePage):
+
+    def findJobResults(self):
+        soup = self._getSoup(features = 'html.parser')
+        results = soup.find_all('a', class_ = 'result-card__full-card-link') 
+
+        return results
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -39,20 +64,13 @@ url_vars = {
     'keywords': search_keywords,
     'location': search_location,
 }
-jobs_url = get_url(url_base, url_vars)
-driver.get(jobs_url)
+main_search_page = LinkedinMainSearchPage.fromArgs(driver, url_base, url_vars)
 time.sleep(1.1231)
 
 # Keep scrolling to the bottom until whole page is rendered
 # Maybe there is a better way of doing this..
-renderWholePage(driver)
-
-# Get the entire html and pass to BeautifulSoup
-html_source = driver.page_source
-soup = BeautifulSoup(html_source,'html.parser')  
+main_search_page.scrollWholePage()
 
 # Find each job result
-results = soup.find_all('a', class_ = 'result-card__full-card-link') 
-print(len(results))
-
-# driver.quit()
+all_results = main_search_page.findJobResults()
+print(len(all_results))
